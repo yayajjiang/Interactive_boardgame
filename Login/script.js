e570d3cdf9cffa60e52dcb7dbbfd4989002b7d39
pika-chu11 <xu36241579@gmail.com>
2023-06-24 22:49:14 -0700

    Start up Login page and user page

---
+++ Login/script.js
@@ -0,0 +1,13 @@
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    if (username === 'admin' && password === 'password') {
+      document.getElementById('login-form').style.display = 'none';
+      document.getElementById('home-page').style.display = 'block';
+      document.getElementById('user').textContent = username;
+    } else {
+      alert('Invalid username or password');
+    }
+  }
+  
