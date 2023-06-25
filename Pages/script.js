2bbd24c8be0afa6bf2784a769770d1a6d333ae02
pika-chu11 <xu36241579@gmail.com>
2023-06-25 02:20:03 -0700

    Updated files layout

--- Pages/script.js
+++ Pages/script.js
@@ -3,12 +3,12 @@ function login() {
     const password = document.getElementById('password').value;
   
     if (username === 'admin' && password === 'password') {
-      window.location.href = '../UserPage/index.html';
+      window.location.href = 'UserPage/index.html';
     } else {
       alert('Invalid username or password');
     }
   }
   
   function SignUp() {
-    window.location.href = '../SignUp/index.html';
+    window.location.href = 'SignUp/index.html';
   }
